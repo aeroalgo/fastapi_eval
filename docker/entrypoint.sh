c028737e6d8747cd78fad03b1b51b76aeab90953
Andrey Erokhin <aeroalgo@gmail.com>
2022-10-21 07:32:35 +0300

    refactoring dockerfile app ports

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 set -e
-python -m gunicorn main:app --bind 0.0.0.0:8003 -w 4 -k uvicorn.workers.UvicornWorker
+python -m gunicorn main:app --bind 0.0.0.0:$APP_PORT -w 4 -k uvicorn.workers.UvicornWorker
