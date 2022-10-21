d22bf212a3fa19af4ec136d264127938a25df290
Andrey Erokhin <aeroalgo@gmail.com>
2022-10-21 03:34:13 +0300

    refactoring env variable

--- app/backend/src/core/settings/conf.py
+++ app/backend/src/core/settings/conf.py
@@ -1,13 +1,19 @@
+import os
+import sys
+
 from environs import Env
 from pydantic import BaseSettings
 
 env = Env()
 env.read_env()
+if not os.environ.get("PYTHONPATH"):
+    os.environ["PYTHONPATH"] = sys.path[0]
+
 
 class Settings(BaseSettings):
     """Класс настроек проекта"""
     APP_PORT: int
-    PYTHONPATH: str
+    PYTHONPATH: str = env("PYTHONPATH")
 
 
 setting = Settings()
--- app/backend/src/main.py
+++ app/backend/src/main.py
@@ -1,8 +1,5 @@
-import os
-
 import uvicorn
 from fastapi import FastAPI
-import sys
 from app.backend.src.core.logger.logg import logger
 from app.backend.src.core.settings.conf import setting
 from app.backend.src.eval.controllers.api import router as api
@@ -12,6 +9,4 @@ app = FastAPI()
 app.include_router(api)
 
 if __name__ == "__main__":
-    # print(os.path.abspath(__file__))
-    # print(sys.path)
     uvicorn.run(app, host="0.0.0.0", port=setting.APP_PORT)
